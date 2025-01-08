5db287ce2612facafb56b7b829899d8f016eeb79
Álex <arsennkn34@gmail.com>
2025-01-08 18:57:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,9 +59,10 @@ if ( url.includes("posts/") ) {
 //Generate the Header HTML, a series of list items containing links.
 let headerHTML = '<ul> <li><a href="' + relativePath + '/index.html">Home</a></li>' + 
 '<li><a href="' + relativePath + '/about.html">About</a></li>' +
+'<li style="text-align: left;">Arsen\'s website (w/blog)!</li>' +
 '<li><a href="' + relativePath + '/projects.html">Projects</a></li>' +
-'<li><a href="' + relativePath + '/archive.html">Blog Archive</a></li>' +
-'<li style="text-align: left;">Arsen\'s website (w/blog)!</li></ul>';
+'<li><a href="' + relativePath + '/archive.html">Blog Archive</a></li></ul>' ;
+
 
 //Generate the Footer HTML, which uses the variables defined in the BASIC INFO section above to list info about the site.
 //Note: feel free to remove the references to Zonelets and Neocities! Just be careful not to delete any necessary HTML closing tags or other syntax.
